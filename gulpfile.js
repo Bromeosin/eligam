6a683c5255a7d415179c71065d74d99192d5f2e2
Billy <Billy@seedpublicity.co.uk>
2018-02-21 22:46:55 +0000

    made desktop nav - working on mobile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,8 @@
-'use strict';
- 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
-gulp.task('default', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+
+gulp.task('default', function(){
+  return gulp.src('assets/css/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(''))
 });
