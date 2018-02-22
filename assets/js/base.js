ddb7fddc0a3a059f0a08ab06aff69e6ab5379c07
SeedWebsite <Billy@seedpublicity.co.uk>
2018-02-22 15:37:25 +0000

    finished mobile nav

--- assets/js/base.js
+++ assets/js/base.js
@@ -3,12 +3,6 @@ $(document).ready(function() {
     $("#toggle").click(function() {
       $(this).toggleClass("active");
       $("#overlay").toggleClass("open");
-
-      var img1 = "images/logo.png";
-      var img2 = "images/logo-light.png";
-      var imgElement = document.getElementById('logo');
-
-      imgElement.src = (imgElement.src === img1)? img2 : img1;
     });
 });
 
@@ -16,11 +10,11 @@ $(document).ready(function(){
   var flag = 1;
   $("#toggle").click(function(){
     if(flag == 0) {
-      $("#logo").attr("src","http://joanna-turner.co.uk/wp-content/themes/joanna-website/images/logo.png");
+      $("#logo").attr("src","assets/images/logo.png");
       flag = 1;
     }
     else if(flag == 1) {
-      $("#logo").attr("src","http://joanna-turner.co.uk/wp-content/themes/joanna-website/images/logo-light.png");
+      $("#logo").attr("src","assets/images/logo.png");
       flag = 0;
     }
   });
